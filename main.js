7d499bec3007c0230a8e81faf4601a7a1d6d48d6
Ottokingstedt <otto.kingstedt@gmail.com>
2021-10-25 12:00:00 +0200

    första data

---
+++ main.js
@@ -0,0 +1,67 @@
+/*
+if (10=="10") {
+    console.log("den h'r raden kommer att köras")
+}
+else {
+
+}
+
+var nameList = ["Otto", "Moa", "Karina"]
+
+console.log(namnList[0])
+console.log(namnList[1])
+console.log(namnList[2])
+
+//
+for (let i = o; i<nameList.length; i++){
+
+    console.log(namnList[i])
+
+}
+
+function skrivaUt () {
+    console.log ("we're trying to print some")
+}
+
+skrivUt ()
+
+function addNumber (a, b){
+
+    console.log (a+b)
+}
+
+var returnedValue = addNumber (2000, 200)
+
+console.log ("return value", "returnedValue")
+
+// 
+
+const listOne = document.querySelector("#list-01")
+console.log (Number(listOne.textContent))
+
+
+const section = document.createElement("section")
+const body = document.querySelector("body")
+body.appendChild (section)
+section.textContent =" hello from section "
+
+//
+
+const ul = document.querySelector ("ul")
+
+const listTwo = document.querySelector ("list-02")
+
+ul.removeChild (listTwo);
+*/
+
+
+const btn = document.querySelector (".btn");
+
+function eventListen () {
+
+    console.log ("hello")
+}
+
+btn.addEventListener ("click", eventListen)
+
+// 
